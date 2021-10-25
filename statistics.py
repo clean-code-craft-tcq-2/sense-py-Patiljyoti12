226c2deb62b5a2e35d7e8d711571ff96624a5400
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2021-10-25 14:18:03 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,13 +1,13 @@
 
 def calculateStats(numbers):
   val=len(numbers)
-    if val==0:
-        return None
-    else:
-        max_val=max(numbers)
-        min_val=min(numbers)
-        avg=sum(numbers)/val
-        return max_val,min_val,avg
+  if val==0:
+    return None
+  else:
+    max_val=max(numbers)
+    min_val=min(numbers)
+    avg=sum(numbers)/val
+    return max_val,min_val,avg
 
 numbers=[1.5, 8.9, 3.2, 4.5]
 print(calculateStats(numbers))
